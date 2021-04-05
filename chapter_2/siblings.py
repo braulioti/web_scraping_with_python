4b4a7c748a748bb16b7ff2fb4aaa74d5f1d1b8f0
Bráulio Figueiredo <braulio@braulioti.com.br>
2021-04-05 11:35:40 -0300

    Tests using parents

---
+++ chapter_2/siblings.py
@@ -0,0 +1,9 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen('http://pythonscraping.com/pages/page3.html')
+bs = BeautifulSoup(html.read(), 'html.parser')
+
+for child in bs.find('table', {'id':'giftList'}).tr.next_siblings:
+    print(child)
+
